c9c08d7e292d4a098b961caacd44abcc62cc8c23
gayathrichintala <gayathrichintala04@gmail.com>
2024-06-09 22:46:33 +0530

    Add files via upload

---
+++ ArithassignEx.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class ArithassignEx {
+
+	public static void main(String[] args) {
+		int a=10,b=3;
+		a+=b;//a-a+b
+		System.out.println(a);
+	    a-=b;//a-a-b
+		System.out.println(a);
+		a*=b;//a-a*b
+		System.out.println(a);
+		a/=b;//a-a/b
+		System.out.println(a);
+		a%=b;//a-a%b
+		System.out.println(a);
+
+	}
+
+}
